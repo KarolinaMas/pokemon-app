575ee55e14543949fecde57457055b5a67fe24fd
Karolina Maščinskaitė <kmkarole@yahoo.com>
2026-03-03 16:55:29 +0200

    add live search

--- src/components/PokemonSearchForm.jsx
+++ src/components/PokemonSearchForm.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { FaSearch } from "react-icons/fa";
 
-const PokemonSearchForm = () => {
+const PokemonSearchForm = ({ onSearch }) => {
+  const { register, watch } = useForm();
+  const searchTerm = watch("search", "");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onSearch(searchTerm);
+    }, 300);
+
+    return () => clearTimeout(timeout);
+  }, [searchTerm, onSearch]);
+
   return (
-    <form action="">
-      <input type="text" />
+    <form className="flex gap-4 items-center justify-center">
+      <input
+        className="shrink-0 w-70 border border-gray-500 rounded-full py-2 px-4"
+        type="text"
+        placeholder="search pokemon"
+        {...register("search")}
+      />
+      <FaSearch cursor="pointer" />
     </form>
   );
 };
